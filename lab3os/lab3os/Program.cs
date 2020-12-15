9bd69f995a90b825bd7d01538e77635af331f5a9
Glassgow666 <45860787+Glassgow666@users.noreply.github.com>
2020-12-15 14:14:09 +0300

    Add files via upload

---
+++ lab3os/lab3os/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab3os
+{
+
+class GFG
+    {
+        static void stack_push(Stack<int> stack)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                stack.Push(i);
+            }
+        }
+
+        
+        static void stack_pop(Stack<int> stack)
+        {
+            Console.WriteLine("Pop :");
+
+            for (int i = 0; i < 5; i++)
+            {
+                int y = (int)stack.Pop();
+                Console.WriteLine(y);
+            }
+        }
+
+    
+        static void stack_peek(Stack<int> stack)
+        {
+            int element = (int)stack.Peek();
+            Console.WriteLine("Element on stack top : " + element);
+        }
+
+       
+        static void stack_search(Stack<int> stack, int element)
+        {
+            bool pos = stack.Contains(element);
+
+            if (pos == false)
+                Console.WriteLine("Element not found");
+            else
+                Console.WriteLine("Element is found at position " + pos);
+        }
+
+        public static void Main(String[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack_push(stack);
+            stack_pop(stack);
+            stack_push(stack);
+            stack_peek(stack);
+            stack_search(stack, 2);
+            stack_search(stack, 6);
+        }
+    }
+
+}
+
